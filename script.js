1714bda015f726ef0f192451b1bf70eab9f95e1f
Albert <albertadiwangsyah@gmail.com>
2024-03-31 16:17:06 +0700

    🎉Upload the solution

---
+++ script.js
@@ -0,0 +1,14 @@
+const cardHolderNameMessage = document.querySelector(
+    ".cardholder__name__error__message"
+);
+const cardNumberMessage = document.querySelector(
+    ".cardnumber__name__error__message"
+);
+const cardDateMessage = document.querySelector(
+    ".cardnumber__format__error__message"
+);
+const cardCVCMessage = document.querySelector(".cvc__error__message");
+const cardHolderInput = document.querySelector("#cardholder-name");
+const cardNumberInput = document.querySelector("#card-number");
+const cardDateInput = document.querySelector("#card-date");
+const cardCVCInput = document.querySelector("#cvc");
